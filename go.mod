a939ecc2158c63e9364645ae3a144ad24cdf1510
Néfix Estrada <nefixestrada@gmail.com>
2020-09-04 01:44:41 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nefixestrada/protoc-gen-go-grpc-mock
+
+go 1.14
+
+require google.golang.org/protobuf v1.25.0
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+const version = "v0.1.0"
+
+func main() {
+	var flags flag.FlagSet
+	protogen.Options{ParamFunc: flags.Set}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+
+		for _, f := range gen.Files {
+			if f.Generate {
+				generateFile(gen, f)
+			}
+		}
+
+		return nil
+	})
+}
